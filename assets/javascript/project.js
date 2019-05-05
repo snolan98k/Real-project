c56598d969ac9f68da6dfc6189d52cf1b9f48754
snolan98k <snolan98k@gmail.com>
2019-05-05 13:26:48 -0400

    api

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,1 +1,15 @@
 console.log("new branch");
+//on click search wig//
+$(document).on('click', '.wig', function () {
+    console.log("inside click event");
+    var wig = $(this).data('style');
+    console.log(`this is the wig that is clicked ${wig}`);
+    var APIKey = "nnOKkLLCfXxXvHh0cEtEEWuiQfgee81wE6ecrtrwOWo2H8UrcVvYEKYZGsCjRkxGjO1gmy0VWoQz7pmg4y6kyDbK5or4vsoCmSjl1VFOhncbkX7Lf10zre-IobfNXHYx";
+    var queryURL = "https://cors-anywhere.herokuapp.com/http://nnOKkLLCfXxXvHh0cEtEEWuiQfgee81wE6ecrtrwOWo2H8UrcVvYEKYZGsCjRkxGjO1gmy0VWoQz7pmg4y6kyDbK5or4vsoCmSjl1VFOhncbkX7Lf10zre-IobfNXHYx";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    }); 
